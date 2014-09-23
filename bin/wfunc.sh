0fc6ceee995d40a9badd9963a1b065178d906ca5
wanchang.ryu <wanchang.ryu@lge.com>
2014-09-23 09:21:10 +0900

    use common cache file for both aroot and broot

--- bin/wfunc.sh
+++ bin/wfunc.sh
@@ -5,24 +5,24 @@ then
     echo ~/.wtools created
 fi
 function aroot {
-    gotodir "vendor/lge/external/chromium34_lge/src" "ace_root_cache"
+    gotodir "vendor/lge/external/chromium34_lge/src"
 }
 function broot {
-    gotodir "vendor/lge/apps/Browser4_KLP" "browser_root_cache"
+    gotodir "vendor/lge/apps/Browser4_KLP"
 }
 
 function gotodir {
     android_root=$(get_android_root)
     target_dir=$1
-    cache_file=~/.wtools/$2
+    cache_file=~/.wtools/working_android_root
     if [[ $android_root == "/" && -f $cache_file ]]
     then
-        cd $(cat $cache_file)
+        cd $(cat $cache_file)/$target_dir
     elif [[ $android_root != "/" ]]
     then
         full_dir=$android_root/$target_dir
         cd $full_dir
-        echo $full_dir > $cache_file
+        echo $android_root > $cache_file
     fi
 }
 
