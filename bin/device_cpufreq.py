2d9194bd905ee095aa078fbab00c5602ed73a921
wanchang.ryu <wanchang.ryu@lge.com>
2015-04-23 19:21:05 +0900

    adb: fixed missing import sys device_cpufreq.py: improve detecting cores and handling non-rooted device

--- bin/device_cpufreq.py
+++ bin/device_cpufreq.py
@@ -1,62 +1,57 @@
-#!/bin/python
+#!/usr/bin/python
 import sys, time,os
 import subprocess
 import re
 import argparse
 
-def make_root():
-    cmd = ["adb","root"]
-    output = subprocess.check_output(cmd)
-    print output
-    #cmd = ["adb","wait-for-device"]
-    #output = subprocess.check_output(cmd)
-    cmd = ["adb","remount"]
-    output = subprocess.check_output(cmd)
-    print output
-
-# check busybox
-def check_busybox():
-    cmd = ["adb","shell","ls -l /system/xbin/busybox"]
-    print "checking busybox...",
-    output = subprocess.check_output(cmd)
+def adb_shell(cmd):
+    adb_cmd = ["adb","shell",cmd]
+    output = subprocess.check_output(adb_cmd)
     if args.verbose:
         print "[V] cmd=", cmd, " output=", output
+    return output;
+
+def check_file(path):
+    print "checking %s..." % (path)
+    output = adb_shell("ls -l %s" % path)
     found = False
     if "No such file or directory" not in output:
-        print "ok"
-        print "The logs saved in /sdcard/cpufreq.log"
+        print "%s ... ok" % (path)
         found = True
     else:
-        print "nok (Please push busybox to /system/xbin)"
+        print "%s ... nok" % (path)
     return found
 
-def make_device_script():
-    # retrieve cpu info
-    cmd = ["adb","shell","cat /proc/cpuinfo | grep -c processor"]
+def make_root():
+    rooted = False
+    cmd = ["adb","root"]
     output = subprocess.check_output(cmd)
-    if args.verbose:
-        print "[V] cmd=", cmd, " output=", output
+    if output == "restarting adbd as root" or output == "adbd is already running as root":
+        rooted = True
+    return rooted
+
+def make_device_script(rooted):
+    # retrieve cpu info
+    output = adb_shell("cat /proc/cpuinfo | grep -c processor")
     cpu_num=int(output)
     print "%d cpu(s) detected" % cpu_num
 
     # retrieve cpu clustering information
     cpu_cluster = {}
+    max_freq_to_cpu = {}
+    unresolved = []
     for i in range(cpu_num):
-        cmd = ["adb","shell","ls","-l","-d","/sys/devices/system/cpu/cpu%d/cpufreq" % i]
-        cpufreq_dir = subprocess.check_output(cmd)
-        if ">" not in cpufreq_dir:
-            cmd = ["adb","shell","cat","/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq" % i]
-            cpuinfo_max_freq = subprocess.check_output(cmd)
-            cmd = ["adb","shell","cat","/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor" % i]
-            scaling_governor = subprocess.check_output(cmd)
-            cpu_cluster[i]={'cpuinfo_max_freq':int(cpuinfo_max_freq), 'scaling_governor':scaling_governor.strip(), 'cpu':[i]}
-    for i in range(cpu_num):
-        cmd = ["adb","shell","ls","-l","-d","/sys/devices/system/cpu/cpu%d/cpufreq" % i]
-        cpufreq_dir = subprocess.check_output(cmd)
-        if ">" in cpufreq_dir:
-            m = re.search("cpu(.)/cpufreq",cpufreq_dir)
-            link_cpu = int(m.group(1))
-            cpu_cluster[link_cpu]['cpu'].append(i)
+        cpuinfo_max_freq = adb_shell("cat /sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq" % i).strip()
+        if "No such file or directory" in cpuinfo_max_freq:
+            unresolved.append(i)
+        else:
+            scaling_governor = adb_shell("cat /sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor" % i).strip()
+            if max_freq_to_cpu.has_key(cpuinfo_max_freq) == False:
+                max_freq_to_cpu[cpuinfo_max_freq]=i
+            if cpu_cluster.has_key(max_freq_to_cpu[cpuinfo_max_freq]):
+                cpu_cluster[max_freq_to_cpu[cpuinfo_max_freq]]['cpu'].append(i)
+            else:
+                cpu_cluster[i]={'cpuinfo_max_freq':int(cpuinfo_max_freq), 'scaling_governor':scaling_governor, 'cpu':[i]}
 
     # make head message
     headstr = ""
@@ -77,15 +72,13 @@ def make_device_script():
             script += "cpu_max_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq 2>/dev/null || echo 0)\n" % (i,i)
             script += "scaling_max_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq 2>/dev/null || echo 0)\n" % (i,i)
             script += "cpuinfo_cur_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq 2>/dev/null || echo 0)\n" % (i,i)
+            script += "scaling_cur_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq 2>/dev/null || echo 0)\n" % (i,i)
         else:
             script += "cpuinfo_cur_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq 2>/dev/null || echo 0)\n" % (i,i)
+            script += "scaling_cur_freq[%d]=$(cat /sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq 2>/dev/null || echo 0)\n" % (i,i)
 
     script += 'format=""\n'
     script += 'args=""\n'
-
-    cpu_list = [ cpu for k in cpu_cluster.keys() for cpu in cpu_cluster[k]['cpu'] ]
-
-    print cpu_list
 
     for i in range(cpu_num):
         script += 'if [ -n "${cpu_max_freq[%d]}" ]; then\n' % i
@@ -94,14 +87,24 @@ def make_device_script():
         script += '    else\n'
         script += '        format+="\e[91m%-10s\e[31m%-10s"\n'
         script += '    fi\n'
-        script += '    args+=" ${scaling_max_freq[%d]} ${cpuinfo_cur_freq[%d]}"\n' % (i,i)
+        if rooted:
+            script += '    args+=" ${scaling_max_freq[%d]} ${cpuinfo_cur_freq[%d]}"\n' % (i,i)
+        else:
+            script += '    args+=" ${cpu_max_freq[%d]} ${scaling_cur_freq[%d]}"\n' % (i,i)
         script += "else\n"
         script += '    format+="%-10s"\n'
-        script += '    args+=" ${cpuinfo_cur_freq[%d]}"\n' % (i)
+        if rooted:
+            script += '    args+=" ${cpuinfo_cur_freq[%d]}"\n' % (i)
+        else:
+            script += '    args+=" ${scaling_cur_freq[%d]}"\n' % (i)
         script += "fi\n"
 
-    script += 'printf $format $args\n'
-    script += "echo \n"
+    if rooted:
+        script += 'printf $format $args\n'
+        script += "echo \n"
+    else:
+        script += 'echo $args\n'
+
     script += "sleep $1\n"
     script += "done\n"
     f = open("cpufreq_d","w")
@@ -109,24 +112,8 @@ def make_device_script():
     f.close()
 
 def install_device_script():
-    os.system("adb push cpufreq_d /system/xbin/cpufreq")
-    os.system("adb shell chmod 777 /system/xbin/cpufreq")
-
-
-def check_device_script():
-    print "script ...",
-    cmd = ["adb","shell","ls -l /system/xbin/cpufreq"]
-    output = subprocess.check_output(cmd)
-    if args.verbose:
-        print "[V] cmd=", cmd, " output=", output
-    found = False
-    if "No such file or directory" not in output:
-        print "ok"
-        found = True
-    else:
-        print "nok"
-    return found
-
+    os.system("adb push cpufreq_d /data/local/tmp/cpufreq")
+    os.system("adb shell chmod 777 /data/local/tmp/cpufreq")
 
 def main():
     global args
@@ -136,17 +123,16 @@ def main():
     parser.add_argument("--force","-f", action='store_true', help='always generate new script and install')
     args = parser.parse_args()
 
-    logging = check_busybox()
+    logging = check_file("/system/xbin/busybox")
 
-    if args.force or check_device_script() == False:
-        make_root()
-        make_device_script()
+    if args.force or check_file("/data/local/tmp/cpufreq") == False:
+        make_device_script(make_root())
         install_device_script()
 
     if logging == True:
-        os.system("adb shell 'cpufreq %.1f| busybox tee /sdcard/cpufreq.log'" % (args.interval/1000))
+        os.system("adb shell '/data/local/tmp/cpufreq %.1f| busybox tee /sdcard/cpufreq.log'" % (args.interval/1000))
     else:
-        os.system("adb shell 'cpufreq $.1f'"% (args.interval/1000))
+        os.system("adb shell '/data/local/tmp/cpufreq %.1f'"% (args.interval/1000))
 
 if __name__ == "__main__":
     main()
